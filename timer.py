7d695786630ab15a5c468d0a0e992180ae89e08d
WilsonZhong01 <38010120+WilsonZhong01@users.noreply.github.com>
2018-04-11 11:42:34 -0400

    new version

--- timer.py
+++ timer.py
@@ -1,19 +1,25 @@
 import threading
 import time
 
-# global timer
-# global count
-# count = 1
+# count = 0
 def main():
-    global timer, count
-    print('timer run every 1 min, and this is the %s' %count +' time')
+    global count, timer
     count += 1
+    print('timer runs every 1 min, and this is the %s' %count +' time')
+
+    # rebuild timer
     timer = threading.Timer(1,main)
     timer.start()
 
-if __name__ == '__main__':
-    count = 1
-    timer = threading.Timer(1,main)
+def timer_fun():
+    timer = threading.Timer(1, main)
     timer.start()
-    time.sleep(3)
+
+
+if __name__ == '__main__':
+    # run fun every 1 min
+    count = 0
+    timer_fun()
+    time.sleep(5 * 1)
     timer.cancel()
+
