c0e1ee5ec170480ed5f12d4598aace5eb83a6c1e
WilsonZhong01 <38010120+WilsonZhong01@users.noreply.github.com>
2018-04-16 16:46:16 -0400

    Add files via upload

--- infoCollectRobot.py
+++ infoCollectRobot.py
@@ -1,22 +1,25 @@
-#-*- coding:UTF-8 -*-
+# -*- coding:UTF-8 -*-
 import itchat
-from itchat.content import TEXT, SHARING,SYSTEM
+from itchat.content import TEXT, SHARING, SYSTEM
 import pymysql, time, pdb
 
 
 class DB:
     conn = None
+
     def __init__(self):
-        self.conn = pymysql.connect(host='localhost', user='root',passwd='',db='mq_admin',port=3306, charset="utf8")
+        self.conn = pymysql.connect(host='localhost', user='root', passwd='', db='mq_admin', port=3306, charset="utf8")
         # self.conn = pymysql.connect(host='199.167.138.175', user='wilson', passwd='Hdf23Bxe', db='mq_admin', port=3306)
-    def queryBySql(self,sql):
+
+    def queryBySql(self, sql):
         cursor = self.conn.cursor()
-        result=cursor.fetchmany(cursor.execute(sql))
+        result = cursor.fetchmany(cursor.execute(sql))
         self.conn.commit()
         return result
 
     def __del__(self):
-        self.conn.close() # close database connection
+        self.conn.close()  # close database connection
+
 
 db = DB()
 
@@ -30,22 +33,22 @@ GroupInfo = itchat.get_chatrooms()
 
 # test different funcitons within contact.py
 
-pdb.set_trace()
-friendsInfo = itchat.get_friends()
+# pdb.set_trace()
 # GroupNickName = GroupInfo['GroupNickName']
 for group in GroupInfo:
     # get member list for each group
     detailedChatroom = itchat.update_chatroom(group['UserName'], detailedMember=True)
+    pdb.set_trace()
     print(detailedChatroom)
-    query = "INSERT INTO group_info(group_name, member_num) VALUES('%s', '%s')" % (group['NickName'], group['MemberCount'])
+    query = "INSERT INTO group_chat_content(group_id, member_num) VALUES('%s', '%s')" % (group['NickName'], group['MemberCount'])
 
     db.queryBySql(query)
-pdb.set_trace()
+# pdb.set_trace()
 
 
 # collecting group chat info
 
-@itchat.msg_register([TEXT,SHARING], isGroupChat=True)
+@itchat.msg_register([TEXT, SHARING], isGroupChat=True)
 def group_text(msg):
     global MsgContent
     # which group message in
@@ -61,7 +64,9 @@ def group_text(msg):
     elif msg['Type'] == SHARING:
         MsgContent = msg['Text']
         print(MsgContent)
-    query = "INSERT INTO group_chat_content(group_id,user_name,content,content_type, time) VALUES('%s', '%s', '%s', '%s', UNIX_TIMESTAMP())" % (chatRoom_id, currentUserName, MsgContent, msg['Type'])
+    query = "INSERT INTO group_chat_content(group_id,user_name,content,content_type, time) VALUES('%s', '%s', '%s', '%s', UNIX_TIMESTAMP())" % (
+    chatRoom_id, currentUserName, MsgContent, msg['Type'])
     db.queryBySql(query)
+
 
 itchat.run()
